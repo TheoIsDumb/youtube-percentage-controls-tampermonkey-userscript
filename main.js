d0cebe039f73993d0ad47d7c55026e95d5f0d2b2
theo <lesgooo1993@proton.me>
2025-05-15 11:39:23 +0530

    container now placed at top-center; added playback speed indicator

--- main.js
+++ main.js
@@ -17,11 +17,11 @@
     container.className = "info";
     container.style.position = 'fixed';
     container.style.display = "flex";
-    container.style.gap = "0.5rem";
-    container.style.bottom = '1rem';
-    container.style.right = '1rem';
-    container.style.zIndex = '1000';
-    container.style.fontSize = '1.5rem';
+    container.style.top = '1rem';
+    container.style.left = '50%';
+    container.style.transform = "translateX(-50%)";
+    container.style.zIndex = '3000';
+    container.style.fontSize = '1rem';
     container.style.color = 'white';
     container.style.fontFamily = "GitLab Sans";
     container.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
@@ -30,17 +30,25 @@
 
     const percentageDisplay = document.createElement('h1');
     percentageDisplay.style.margin = '0';
+    percentageDisplay.style.paddingRight = '1rem';
     container.appendChild(percentageDisplay);
+
+    const speedText = document.createElement('h1');
+    speedText.style.margin = '0';
+    speedText.style.paddingRight = '0.5rem';
+    speedText.innerText = '1x';
+    speedText.style.paddingRight = '1rem';
+    container.appendChild(speedText);
 
     const speeds = [1, 1.25, 1.5, 1.75, 2, 3];
     speeds.forEach(speed => {
         const button = document.createElement('button');
         button.textContent = `${speed}x`;
-        button.style.margin = '5px';
-        button.style.fontSize = '1rem';
+        button.style.fontSize = '1.2rem';
         button.style.cursor = 'pointer';
         button.onclick = () => {
             video.playbackRate = speed;
+            speedText.textContent = `${speed}x`;
         };
         container.appendChild(button);
     });
@@ -49,7 +57,8 @@
 
     setInterval(() => {
         if (video && video.duration > 0) {
-	    percentageDisplay.textContent = Math.round((video.currentTime / video.duration) * 100) + "%";
+            percentageDisplay.textContent = Math.round((video.currentTime / video.duration) * 100) + "%";
         }
     }, 1000);
 })();
+
